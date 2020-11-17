a9a3cf422b23d8f4336780d43983035f718cf46e
thilox95e <thilokhimashaac@gmail.com>
2020-11-17 08:51:32 +0000

    Hello Thilok

--- src/main/java/org/pictolearn/docker/HelloWorldPing.java
+++ src/main/java/org/pictolearn/docker/HelloWorldPing.java
@@ -3,8 +3,8 @@ package org.pictolearn.docker;
 public class HelloWorldPing {
 	
 	public static void main(String args[]) throws Exception{
-		for(int i = 0 ; i < 100; i++){
-			System.out.println("Hello World Ping " + i );
+		for(int i = 0 ; i < 150; i++){
+			System.out.println("Hello Thilok :  " + i );
 			Thread.sleep(1000);
 		}
 	}
